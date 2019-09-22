b4c15b84d28f23bc56c3d9a95d4ce26072f7c637
Daemon Walker <215136855@163.com>
2019-09-23 06:59:12 +0800

    v0.0.1

--- Daem0n.SimIoc/ServiceProvider.cs
+++ Daem0n.SimIoc/ServiceProvider.cs
@@ -35,23 +35,31 @@ namespace Daem0n.SimIoc
             throw new NotImplementedException();
         }
 
-        public object Get(Type type)
+        public object Get(Type tSource)
         {
-            if (singleton.Contains(type))
+            if (singleton.Contains(tSource))
             {
-                var target = singleton.GetTarget(type);
+                return GetSingleton(tSource);
             }
-            else if (scoped.Contains(type))
+            else if (scoped.Contains(tSource))
             {
-
+                return GetScoped(tSource);
             }
-            else if (transient.Contains(type))
+            else if (transient.Contains(tSource))
             {
-
+                return GetTransient(tSource);
             }
-            else if (type.IsConstructedGenericType)
+            else if (tSource.IsConstructedGenericType)
             {
+                var outType = tSource.GetGenericTypeDefinition();
+                if (outType == typeof(IEnumerable<>))
+                {
 
+                }
+                else
+                {
+
+                }
             }
             else
             {
@@ -63,7 +71,7 @@ namespace Daem0n.SimIoc
             var tTarget = singleton.GetTarget(tSource);
             if (singletonObjects.TryGetValue(tTarget, out var obj) == false)
             {
-                obj = this.CreateObject(singleton.GetBuilder(tTarget));
+                obj = this.CreateObject(tSource, tTarget, singleton.GetBuilder(tTarget));
                 if (singletonObjects.TryAdd(tTarget, obj) == false)
                 {
                     singletonObjects.TryGetValue(tTarget, out obj);
@@ -74,15 +82,15 @@ namespace Daem0n.SimIoc
         private object GetTransient(Type tSource)
         {
             var tTarget = transient.GetTarget(tSource);
-            return this.CreateObject(transient.GetBuilder(tTarget));
+            return this.CreateObject(tSource, tTarget, transient.GetBuilder(tTarget));
         }
         private object GetScoped(Type tSource)
         {
             var tTarget = scoped.GetTarget(tSource);
             var key = new Tuple<Thread, Type>(Thread.CurrentThread, tSource);
-            if (scopedObjects.TryGetValue(key, out var obj))
+            if (scopedObjects.TryGetValue(key, out var obj) == false)
             {
-                obj = this.CreateObject(scoped.GetBuilder(tTarget));
+                obj = this.CreateObject(tSource, tTarget, scoped.GetBuilder(tTarget));
                 if (scopedObjects.TryAdd(key, obj) == false)
                 {
                     scopedObjects.TryGetValue(key, out obj);
@@ -108,23 +116,101 @@ namespace Daem0n.SimIoc
         }
         private object CreateObject(Type t)
         {
-            var constructors = t.GetConstructors().Where(p => p.IsPublic == true).ToList();
+            var constructors = t.GetConstructors().Where(p => p.IsPublic == true).ToArray();
             ConstructorInfo constructor = null;
             ParameterInfo[] parms = null;
-            if (constructors.Count == 0)
+            if (constructors.Length == 0)
             {
                 throw new Exception("No Constructors");
             }
-            if (constructors.Count == 1)
+            if (constructors.Length == 1)
             {
                 constructor = constructors.First();
                 parms = constructor.GetParameters();
             }
             else
             {
+                Array.Sort(constructors,
+                    (c1, c2) => c2.GetParameters().Length.CompareTo(c1.GetParameters().Length));
+                constructor = constructors.First();
+                var hsParms = new HashSet<ParameterInfo>(constructor.GetParameters());
+                for (var i = 1; i < constructors.Length; i++)
+                {
+                    if (hsParms.IsSupersetOf(constructors[i].GetParameters()) == false)
+                    {
+                        throw new Exception("Invalid Parm");
+                    }
+                }
+                parms = constructor.GetParameters();
+            }
+            //var constructorParms =;
+            return constructor.Invoke(parms.Select(_ => Get(_.ParameterType)).ToArray());
+        }
+        private object CreateGeneric(Type tSource)
+        {
+            var outType = tSource.GetGenericTypeDefinition();
+            if (singleton.Contains(outType))
+            {
+                var target = singleton.GetTarget(outType);
+                target = target.MakeGenericType(tSource.GenericTypeArguments);
+                singleton.Add(tSource, target, null, null);
+                return GetSingleton(tSource);
 
             }
+            else if (scoped.Contains(outType))
+            {
+                var target = scoped.GetTarget(outType);
+                target = target.MakeGenericType(tSource.GenericTypeArguments);
+                scoped.Add(tSource, target, null, null);
+                scopedObjects.TryAdd(new Tuple<Thread, Type>(Thread.CurrentThread, target), CreateObject(target));
+                return GetScoped(tSource);
+            }
+            else if (transient.Contains(outType))
+            {
+                var target = transient.GetTarget(outType);
+                target = target.MakeGenericType(tSource.GenericTypeArguments);
+                transient.Add(tSource, target, (provider) => CreateObject(target), null);
+                return GetTransient(tSource);
+            }
+            else
+            {
+                return null;
+            }
         }
+
+        private object CreateIEnumerable(Type tSource)
+        {
+            var inType = tSource.GenericTypeArguments.Single();
+            var newType = typeof(List<>).MakeGenericType(inType);
+            if (singleton.Contains(inType))
+            {
+                var target = singleton.GetTarget(inType);
+                target = target.MakeGenericType(tSource.GenericTypeArguments);
+                singleton.Add(tSource, target, null, null);
+                return GetSingleton(tSource);
+
+            }
+            else if (scoped.Contains(inType))
+            {
+                var target = scoped.GetTarget(inType);
+                target = target.MakeGenericType(tSource.GenericTypeArguments);
+                scoped.Add(tSource, target, null, null);
+                scopedObjects.TryAdd(new Tuple<Thread, Type>(Thread.CurrentThread, target), CreateObject(target));
+                return GetScoped(tSource);
+            }
+            else if (transient.Contains(inType))
+            {
+                var target = transient.GetTarget(inType);
+                target = target.MakeGenericType(tSource.GenericTypeArguments);
+                transient.Add(tSource, target, (provider) => CreateObject(target), null);
+                return GetTransient(tSource);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
         private bool CheckType(Type tSource)
         {
             if (singleton.Contains(tSource))
@@ -150,6 +236,10 @@ namespace Daem0n.SimIoc
                 {
                     return CheckType(outType);
                 }
+            }
+            else
+            {
+                return false;
             }
         }
     }
